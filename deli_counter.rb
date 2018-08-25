513fc5d4955f6a2643dc8c073fc863e6978b1a8a
Vicente Soriano <vicente.soriano@icloud.com>
2018-08-25 14:31:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@
 def line(array_of_people)
  people_and_place = ""
  array_of_people.each_with_index do |person, place_in_line|
-   people_and_place << "#{place_in_line + 1}. #{person}"
+   people_and_place << "#{place_in_line + 1}. #{person} "
  end
  if array_of_people.size == 0
    puts "The line is currently empty."
