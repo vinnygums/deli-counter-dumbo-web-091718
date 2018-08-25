f37796f46e1660fc786cbb641cd0a3f5160a4352
Vicente Soriano <vicente.soriano@icloud.com>
2018-08-25 14:31:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,9 @@
 # Write your code here.
 
 def line(array_of_people)
- people_and_place = []
+ people_and_place = ""
  array_of_people.each_with_index do |person, place_in_line|
    people_and_place << "#{place_in_line + 1}. #{person}"
-   people_and_place.join(",")
  end
  if array_of_people.size == 0
    puts "The line is currently empty."
