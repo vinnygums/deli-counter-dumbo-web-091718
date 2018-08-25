817e7cf52a2643995f6473f572000794132ce269
Vicente Soriano <vicente.soriano@icloud.com>
2018-08-25 14:52:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,6 +21,6 @@ def now_serving(line)
   if line.size == 0
     puts "There is nobody waiting to be served."
   else
-    puts "Currently serving #{line.shift}."
+    puts "Currently serving."
   end
 end
